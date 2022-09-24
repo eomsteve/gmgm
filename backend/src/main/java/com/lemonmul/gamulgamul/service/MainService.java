e22b90cc16af6e428dcca383980f485c9339c672
Hyo-eun Kim <backji5@gmail.com>
2022-09-25 05:19:23 +0900

    feat: add naver api request function

--- backend/src/main/java/com/lemonmul/gamulgamul/service/MainService.java
+++ backend/src/main/java/com/lemonmul/gamulgamul/service/MainService.java
@@ -1,5 +1,7 @@
 package com.lemonmul.gamulgamul.service;
 
+import com.lemonmul.gamulgamul.api.dto.news.ItemDto;
+import com.lemonmul.gamulgamul.api.dto.news.SearchResultDto;
 import com.lemonmul.gamulgamul.entity.checklist.Checklist;
 import com.lemonmul.gamulgamul.entity.priceindex.IndexType;
 import com.lemonmul.gamulgamul.entity.priceindex.PriceIndex;
@@ -8,7 +10,14 @@ import com.lemonmul.gamulgamul.repo.ChecklistRepo;
 import com.lemonmul.gamulgamul.repo.PriceIndexRepo;
 import com.lemonmul.gamulgamul.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -49,5 +58,62 @@ public class MainService {
     /**
      * 물가 관련 뉴스 조회
      * */
+
+
+
+    /**
+     * 물가 관련 뉴스 API 요청 및 DB 저장
+     * TODO: Cron도 ..해야지..ㅎ...
+     * */
+    public void apiProcess(){
+        // 요청한 api 내용 받아 오는 함수 호출
+        SearchResultDto news = getNews();
+
+        // TODO: db에 기존에 있던 기사 삭제하고 저장하는 함수 호출
+
+        //
+
+    }
+
+    /**
+    * API 요청 결과 body를 반환해 주는 함수
+    * ? private으로 바꿔야 하지 않을까?
+    * */
+    public SearchResultDto getNews(){
+        String clientId = "8dsQLQwo_ame0XieXqz0";
+        String clientSecret = "09sRAUGzaj";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 네이버 API 요청 시 headr에 넣어야 하는 정보 추가
+        HttpHeaders header = new HttpHeaders();
+        header.add("X-Naver-Client-Id", clientId);
+        header.add("X-Naver-Client-Secret", clientSecret);
+        HttpEntity<?> entity = new HttpEntity<>(header);
+
+        // URI 빌드
+        String keyword = "물가";
+        Integer size = 10;
+        UriComponents uri = UriComponentsBuilder.newInstance()
+                .scheme("https").host("openapi.naver.com").path(("/v1/search/news.json"))
+                .query("query={keyword}").query("display={size}")
+                .buildAndExpand(keyword, size);
+
+        // 요청 받기
+        ResponseEntity<SearchResultDto> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, SearchResultDto.class);
+
+        return resultMap.getBody();
+
+    }
+
+    /**
+     * DB 저장 함수
+     * */
+    public void saveNews(List<ItemDto> items){
+        // TODO: Dto 에 있는 정보 뽑아서 entity 생성해서 넣어주기
+        // ? 하나씩 따로따로 저장 vs List 만들어서 entity 때려 담은 후에 save all ?
+
+
+    }
 
 }
