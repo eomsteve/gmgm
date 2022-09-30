98622891a4347b2a911957dc03f20545e4af783d
JDK <1217jdk@naver.com>
2022-09-30 11:15:33 +0900

    feat: modify exception message

--- backend/src/main/java/com/lemonmul/gamulgamul/exhandler/ExControllerAdvice.java
+++ backend/src/main/java/com/lemonmul/gamulgamul/exhandler/ExControllerAdvice.java
@@ -19,15 +19,26 @@ import java.util.NoSuchElementException;
 public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({IllegalArgumentException.class, ExpiredJwtException.class})
+    @ExceptionHandler({IllegalArgumentException.class})
     public ErrorResult badRequestExHandler(Exception e){
-        log.error("[Bad Request Exception Handler] ex",e);
+        log.error("[Exception Handler] ex",e);
         return ErrorResult.builder()
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-//                .message("잘못된 입력값입니다.")
-                .message(e.getMessage())
+                .message("잘못된 입력값입니다.")
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ExpiredJwtException.class})
+    public ErrorResult tokenExpiredExHandler(Exception e){
+        log.error("[Exception Handler] ex",e);
+        return ErrorResult.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message("만료된 토큰입니다.")
                 .build();
     }
 
@@ -39,21 +50,19 @@ public class ExControllerAdvice {
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-//                .message("잘못된 입력값입니다.")
-                .message(e.getMessage())
+                .message("Json 생성에 실패했습니다.")
                 .build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResult authenticationServiceExHandler(AuthenticationServiceException e){
-        log.error("[Authentication Exception Handler] ex",e);
+        log.error("[Exception Handler] ex",e);
         return ErrorResult.builder()
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-//                .message("잘못된 요청 방식입니다.")
-                .message(e.getMessage())
+                .message("인증에 실패했습니다.")
                 .build();
     }
 
@@ -65,21 +74,19 @@ public class ExControllerAdvice {
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-//                .message("해당 값이 존재하지 않습니다.")
-                .message(e.getMessage())
+                .message("해당 값이 존재하지 않습니다.")
                 .build();
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler
-    public ErrorResult noSuchElementExHandler(RestClientException e){
+    public ErrorResult restTemplateExHandler(RestClientException e){
         log.error("[Exception Handler] ex",e);
         return ErrorResult.builder()
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-//                .message("서버 내부 오류가 발생했습니다.")
-                .message(e.getMessage())
+                .message("외부 api 요청에 실패했습니다.")
                 .build();
     }
 
@@ -91,8 +98,7 @@ public class ExControllerAdvice {
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-//                .message("서버 내부 오류가 발생했습니다.")
-                .message(e.getMessage())
+                .message("서버 내부 오류가 발생했습니다.")
                 .build();
     }
 }
