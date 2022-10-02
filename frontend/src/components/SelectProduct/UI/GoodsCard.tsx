ba0316a92631b71c68504754168481883e5b2690
Hyo-eun Kim <backji5@gmail.com>
2022-10-03 05:26:40 +0900

    design: selection color, grid gap, recommendation titlev

--- frontend/src/components/SelectProduct/UI/GoodsCard.tsx
+++ frontend/src/components/SelectProduct/UI/GoodsCard.tsx
@@ -6,16 +6,16 @@ const GoodsCard: FC<GoodsItem> = props => {
   const { goodsId,goodsName,goodsImg  } = props;
   
   return (
-    (goodsName.length <= 8) ? <div className="m-2 grid grid-cols-3 items-center rounded-lg border border-gray-300 p-4 text-[16px]">
+    (goodsName.length <= 8) ? <div className="m-2 grid h-5/6 grid-cols-3 items-center rounded-lg border border-gray-300 p-4 text-[16px]">
     <div className="flex h-12 w-12 justify-center">
-      <img className="p-0" src={goodsImg} />
+      <img className="p-0 rounded-lg" src={goodsImg} />
     </div>
-    <div className="col-span-2 p-1"> {goodsName} </div>
-  </div> :  <div className="m-2 grid grid-cols-3 items-center rounded-lg border border-gray-300 p-3 text-[15.5px]">
+    <div className="col-span-2 p-1 ps-2"> {goodsName} </div>
+  </div> :  <div className="m-2 grid h-5/6 grid-cols-3 items-center rounded-lg border border-gray-300 p-3 text-[15px]">
       <div className="flex h-12 w-12 justify-center">
-        <img className="p-0" src={goodsImg} />
+        <img className="rounded-lg" src={goodsImg} />
       </div>
-      <div className="col-span-2 p-1"> {goodsName} </div>
+      <div className="col-span-2 p-2"> {goodsName} </div>
     </div>
     );
 };
--- frontend/src/components/favorite/RecommendComponent.tsx
+++ frontend/src/components/favorite/RecommendComponent.tsx
@@ -9,9 +9,15 @@ const Recommend: FC<RecommendProps> = props => {
   // const { goodsList } = props;
   return (
     <>
+      <div className="my-2 mt-[1vh] text-lg">
+          이런 상품은 어때요?
+          <span className="ml-2 text-xs text-gray-500">
+            다음 상품들도 좋아하실 것 같아요.
+          </span>
+        </div>
       <div className="border m-3 w-[85vw] h-[66px] text-center text-align-center">
         <p>안녕 난 Recommend Component</p>
-        <p>item은 GoodsCard 재활용하면되지롱</p>
+        <p>item은 GoodsCard 재활용하면 되지롱</p>
       </div>
     </>
   );
