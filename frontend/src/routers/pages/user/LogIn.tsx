5e52b3e1160767a2a67355fc5a9c1b43f5d789a2
zaq1290 <zaq1290@naver.com>
2022-10-03 01:16:02 +0900

    design: header sticky top, login, signin css

--- frontend/src/routers/pages/user/LogIn.tsx
+++ frontend/src/routers/pages/user/LogIn.tsx
@@ -1,8 +1,14 @@
-import {FC} from 'react';
+import { FC } from 'react';
 import LogIn from '../../../components/users/LogInComponent';
+import LogInHeader from '@components/EmptyHeader';
 
-const LogInPage : FC = () => {
-  return <LogIn />
-}
+const LogInPage: FC = () => {
+  return (
+    <>
+      <LogInHeader title="로그인" navigateRouter="" />
+      <LogIn />
+    </>
+  );
+};
 
 export default LogInPage;
