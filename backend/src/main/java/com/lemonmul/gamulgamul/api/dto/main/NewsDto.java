4966ed2dbf947e526af3284ad685d67b03b70bdf
zaq1290 <zaq1290@naver.com>
2022-10-04 12:23:17 +0900

    fix: modify news pubDate, price dto - price to unit price

--- backend/src/main/java/com/lemonmul/gamulgamul/api/dto/main/NewsDto.java
+++ backend/src/main/java/com/lemonmul/gamulgamul/api/dto/main/NewsDto.java
@@ -19,7 +19,7 @@ public class NewsDto {
     public NewsDto(News news) {
         title=news.getTitle();
         link=news.getLink();
-        pubDate=news.getPubDate().format(DateTimeFormatter.ofPattern("yyyy년 M월 d일 h시 m분"));
+        pubDate=news.getPubDate().plusHours(9).format(DateTimeFormatter.ofPattern("yyyy년 M월 d일 h시 m분"));
         log.debug("news dto pubDate: {}",pubDate);
     }
 }
