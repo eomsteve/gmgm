8c480821388425da161f733bf0585a253a789cb6
soyeon Lim <isy5111@naver.com>
2022-10-05 23:15:36 +0900

    css: landingImg fix

--- frontend/src/components/users/LandingPage.tsx
+++ frontend/src/components/users/LandingPage.tsx
@@ -5,7 +5,7 @@ import './LandingPage.css'
 export default function LandingPage() {
   return (
     <div className='fixed bg-white'>
-      <img className='object-fill h-screen fade-out' src={LandingImg} alt='landingImg'></img>
+      <img className='object-fill w-screen h-screen fade-out' src={LandingImg} alt='landingImg'></img>
     </div>
   )
 }
